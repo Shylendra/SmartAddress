8a84327775664d6a05497b0ef19e5fcdb5ac3692
shylendra <shylendra.tm@gmail.com>
2022-02-22 12:38:11 +1100

    Updated appspec.yml

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo 'Fix previleges - SmartAddress'
 chmod +x /home/ec2-user/smartaddress-web-1.0-SNAPSHOT.jar
 chmod +x /home/ec2-user/start-server.sh
 chmod +x /home/ec2-user/stop-server.sh
