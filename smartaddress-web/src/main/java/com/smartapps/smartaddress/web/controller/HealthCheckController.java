1ae74ae0e866eb1c5b635e5e5c23a5db58d24bf8
shylendra <shylendra.tm@gmail.com>
2022-11-04 22:23:20 +1100

    Added HealthCheck

---
+++ smartaddress-web/src/main/java/com/smartapps/smartaddress/web/controller/HealthCheckController.java
@@ -0,0 +1,19 @@
+package com.smartapps.smartaddress.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class HealthCheckController {
+
+	@GetMapping("/")
+	public ResponseEntity<Void> health(){
+		log.info("HealthCheckController::health()::OK");
+		return ResponseEntity.ok().build();
+	}
+	
+}
